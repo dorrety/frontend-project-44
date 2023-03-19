31468442faee1b25b78236e5a7f974d8fc5f727d
dorrety <shev1701@gmail.com>
2023-03-19 10:39:35 +0200

    fix

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -33,7 +33,7 @@ const brainPrime = (randomNumber) => {
             correctSum += 1;
             console.log('Correct!');
             if (correctSum === 3) {
-                console.log(`Congratulations, ${userName}`);
+                console.log(`Congratulations, ${userName}!`);
             }
         } else {
             console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'\nLet's try again, ${userName}`);
