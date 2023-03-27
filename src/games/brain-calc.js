0625a4d20de8f3a710ef12ddd64e2baa5091e4af
dorrety <shev1701@gmail.com>
2023-03-27 12:38:12 +0200

    fix brain-calc

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -1,8 +1,6 @@
 import runGame from '../index.js';
 import getRandomInt from '../randomInt.js';
 
-let correctAnswer;
-
 const rules = 'What is the result of the expression?';
 
 const getRandomOperator = () => {
@@ -29,7 +27,7 @@ const getData = () => {
   const firstRandomInt = getRandomInt(0, 10);
   const secondRandomInt = getRandomInt(0, 10);
   const operator = getRandomOperator();
-  correctAnswer = correctBrainCalcAnswer(operator, firstRandomInt, secondRandomInt).toString();
+  const correctAnswer = correctBrainCalcAnswer(operator, firstRandomInt, secondRandomInt).toString();
   const question = `Question: ${firstRandomInt} ${operator} ${secondRandomInt}`;
   return [question, correctAnswer];
 };
